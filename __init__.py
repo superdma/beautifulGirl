9fe74959ea8a640d77bbcc0706749623174a27fc
ZhaoZhaoMa <2263642065@qq.com>
2019-05-02 12:06:13 +0800

    openCV chinese name and path problem

--- __init__.py
+++ __init__.py
@@ -3,8 +3,7 @@ import json
 import requests
 import os
 import cv2
-from PIL import Image
-
+import numpy as np
 
 class BaiduPicIndentify:
     def __init__(self, img):
@@ -22,14 +21,30 @@ class BaiduPicIndentify:
         return json_result['access_token']
 
     def img_to_BASE64(slef, path):
-        print(path)
-        with open(path, 'rb') as f:
-            base64_data = base64.b64encode(f.read())
-            return base64_data
+        try:
+            with open(path, 'rb') as f:
+                base64_data = base64.b64encode(f.read())
+                return base64_data
+        except FileNotFoundError:
+            print(path)
+            print("文件名称出错，或文件不存在 或路径不正确, 请重新输入所需图片路径")
+            print("=================分割线=====================")
+            return None
+
+    def cv_imread(self):
+        # 解决OpenCV的imread函数无法读取中文路径和中文命名的文件的问题
+        img_path = os.path.abspath(self.img_src)
+        print(img_path)
+        cv_img = cv2.imdecode(np.fromfile(img_path, dtype=np.uint8), -1)
+        # imdecode读取的是rgb，如果后续需要opencv处理的话，需要转换成bgr，转换后图片颜色会变化
+        #cv_img=cv2.cvtColor(cv_img,cv2.COLOR_RGB2BGR)
+        return cv_img
 
     def detect_face(self):
         # 人脸检测与属性分析
         img_BASE64 = self.img_to_BASE64(self.img_src)
+        if img_BASE64 is None:
+            return
         request_url = "https://aip.baidubce.com/rest/2.0/face/v3/detect"
         post_data = {
             "image": img_BASE64,
@@ -57,12 +72,10 @@ class BaiduPicIndentify:
             print("表 情：", face_list[i]['expression']['type'])
             print("眼 镜：", face_list[i]['glasses']['type'])
             print("脸 型：", face_list[i]['face_shape']['type'])
-            #print("位 置：", face_list[i]['location']['left'], face_list[i]['location']['top'],
-            #face_list[i]['location']['width'], face_list[i]['location']['height'] )
             i = i+1
-        img_path = os.path.abspath(self.img_src)
-        img = cv2.imread(img_path)
-        blue = (65, 105, 225)
+
+        img = self.cv_imread()
+        blue = (202, 235, 216)
         # 框的位置为 左上角、右下角的坐标，颜色，填充的样式
         i = 0
         for i in range(0, len(face_list)):
@@ -75,15 +88,19 @@ class BaiduPicIndentify:
             font = cv2.FONT_HERSHEY_COMPLEX
             beauty = str(face_list[i]['beauty'])
             # 字体标注的位置， 内容，字体设置
-            red = (176, 23, 31)
+            red = (255, 0, 0)
             cv2.putText(img, beauty, (left+ int(width/2), top-3), font, 0.5, red, 1)
             i = i + 1
-        cv2.namedWindow(self.img_src, cv2.WINDOW_AUTOSIZE)
-        cv2.imshow(self.img_src, img)
+        cv2.namedWindow("defect_result", cv2.WINDOW_AUTOSIZE)
+        cv2.imshow("defect_result", img)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
-        img_save_path = 'G:/'+self.img_src
-        cv2.imwrite(img_save_path, img)
+
+        (filepath, tempfilename) = os.path.split(self.img_src)
+        (filename, extension) = os.path.splitext(tempfilename)
+        img_save_path = 'G:/img'+tempfilename
+        cv2.imencode('.jpg', img)[1].tofile(img_save_path)
+        #cv2.imwrite(img_save_path, img)
 
 
 if __name__ == '__main__':
